6571d2883ecf37c11a66fb2a561f6905b63ad4de
unknown <marcel_bousquet@hotmail.com>
2022-02-17 16:26:23 -0300

    aula funcoes

---
+++ modulo1/funcoes/index.js
@@ -0,0 +1,26 @@
+/*function minhafuncao(variavel) {
+return variavel * 5
+}
+
+console.log(minhafuncao(2))
+console.log(minhafuncao(10))
+
+//1.a  10 e 50
+//1.b Nada, pois você estaria somente chamando a função mas sem pedir para imprimir no console*/
+
+//let textodousuario = prompt("Insira um texto")
+
+//let outrafuncao = function(texto) {
+//return texto.tolowercase().includes("cenoura")
+
+//}
+
+//const resposta = outrafuncao(textodousuario)
+//console.log(resposta)
+
+//Escreva um arrow function que recebe um parametro e imprime no console esse parametro
+
+let paramentrounico = 5 => {
+console.log(paramentrounico)
+}
+
